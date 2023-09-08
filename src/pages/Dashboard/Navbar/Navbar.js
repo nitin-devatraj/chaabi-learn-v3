c4ad85db7fce4716ac9933bc1ea913acd540ba52
nitin devatraj <nitin.devatraj@chaabi.co>
2023-09-08 15:11:37 +0530

    tweak NavBar component

--- src/pages/Dashboard/Navbar/Navbar.js
+++ src/pages/Dashboard/Navbar/Navbar.js
@@ -1,64 +1,80 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import styles from "./Navbar.module.scss";
 import HomeIcon from "./NavBarIcons/HomeIcon/HomeIcon";
 
+const selectedBtnreducer = (prevState, action) => {
+  switch (action) {
+    case action === "Home":
+      return "Home";
+    case action === "Explore":
+      return "Explore";
+    case action === "Search":
+      return "Search";
+    case action === "Support":
+      return "Support";
+    default:
+      return action;
+  }
+};
+
 function Navbar() {
-  const [selectedButton, setSelectedButton] = useState("Home");
+  const [selectedBtn, dispatchSelectedBtn] = useReducer(
+    selectedBtnreducer,
+    "Home"
+  );
 
   const buttonClickHandler = (event) => {
-    setSelectedButton(event.target.id);
+    dispatchSelectedBtn(event.target.id);
   };
 
   return (
     <nav className={styles.navBarContainer}>
       <button
         className={`${styles.navItem} ${
-          selectedButton === "Home" ? styles.selected : undefined
+          selectedBtn === "Home" ? styles.selected : undefined
         }`}
         onClick={buttonClickHandler}
         id="Home"
       >
-        <HomeIcon
-          fillColor={selectedButton === "Home" ? "#303134" : "#75777A"}
-        />
+        <HomeIcon fillColor={selectedBtn === "Home" ? "#303134" : "#75777A"} />
         Home
       </button>
 
       <button
         className={`${styles.navItem} ${
-          selectedButton === "Explore" ? styles.selected : null
+          selectedBtn === "Explore" ? styles.selected : null
         }`}
         onClick={buttonClickHandler}
         id="Explore"
       >
         <HomeIcon
-          fillColor={selectedButton === "Explore" ? "#303134" : "#75777A"}
+          fillColor={selectedBtn === "Explore" ? "#303134" : "#75777A"}
         />
         Explore
       </button>
 
       <button
         className={`${styles.navItem} ${
-          selectedButton === "Search" ? styles.selected : null
+          selectedBtn === "Search" ? styles.selected : null
         }`}
         onClick={buttonClickHandler}
         id="Search"
       >
         <HomeIcon
-          fillColor={selectedButton === "Search" ? "#303134" : "#75777A"}
+          fillColor={selectedBtn === "Search" ? "#303134" : "#75777A"}
         />
         Search
       </button>
 
       <button
         className={`${styles.navItem} ${
-          selectedButton === "Support" ? styles.selected : null
+          selectedBtn === "Support" ? styles.selected : null
         }`}
         onClick={buttonClickHandler}
         id="Support"
       >
         <HomeIcon
-          fillColor={selectedButton === "Support" ? "#303134" : "#75777A"}
+          fillColor={selectedBtn === "Support" ? "#303134" : "#75777A"}
         />
         Support
       </button>
