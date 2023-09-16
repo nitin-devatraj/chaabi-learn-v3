ccce044d16c1929ad01caec57fc60f7fcaf97348
nitin devatraj <nitin.devatraj@chaabi.co>
2023-09-16 13:21:38 +0530

    refactore SecondaryButton component UI

--- src/components/Buttons/SecondaryButton/SecondaryButton.js
+++ src/components/Buttons/SecondaryButton/SecondaryButton.js
@@ -2,18 +2,19 @@ import React from "react";
 import styles from "./SecondaryButton.module.scss";
 import { useSelector } from "react-redux";
 
-function SecondaryButton({ disabled, onClick, children }) {
+function SecondaryButton({ disabled, onClick, children, className, type }) {
   const isDarkMode = useSelector((state) => state.theme.darkMode);
 
-  const secondaryBtnStyles = isDarkMode
-    ? styles.secondaryBtnDarkTheme
-    : styles.secondaryBtnLightTheme;
+  const secondaryBtnStyles = `${styles.secondaryBtnLightTheme} ${
+    isDarkMode && styles.secondaryBtnDarkTheme
+  } ${className}`;
 
   return (
     <button
       className={secondaryBtnStyles}
       disabled={disabled}
       onClick={onClick}
+      type={type ?? "button"}
     >
       {children}
     </button>
